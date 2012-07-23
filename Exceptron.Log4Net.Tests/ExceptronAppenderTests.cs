2b870ea6818b79ec876997748081b7535e5591c4
Rob Chartier <rchartier@pnimedia.com>
2012-07-23 11:12:27 -0700

    Initial commit

---
+++ Exceptron.Log4Net.Tests/ExceptronAppenderTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Exceptron.Log4Net.Client;
+using log4net;
+using log4net.Core;
+using NUnit.Framework;
+using log4net.Repository.Hierarchy;
+
+namespace Exceptron.Log4Net.Tests
+{
+    [TestFixture]
+    public class ExceptronAppenderTests
+    {
+        [SetUp]
+        public void TestAppender()
+        {
+            Hierarchy hierarchy = (Hierarchy)LogManager.GetRepository();
+            Exceptron.Log4Net.Client.ExceptronAppender ea = new ExceptronAppender();
+            ea.ApiKey = "6db93c30164443e1ab46d7eae2b41dbf";
+
+            hierarchy.Root.AddAppender(ea);
+
+            hierarchy.Root.Level = Level.All;
+            hierarchy.Configured = true;
+
+        }
+        [Test]
+        public void Info()
+        {
+            log4net.LogManager.GetLogger(typeof(ExceptronAppenderTests)).Info("Info");
+        }
+        [Test]
+        public void Debug()
+        {
+            log4net.LogManager.GetLogger(typeof(ExceptronAppenderTests)).Debug("Debug");
+        }
+        [Test]
+        public void Error()
+        {
+            log4net.LogManager.GetLogger(typeof(ExceptronAppenderTests)).Error("Error");
+        }
+        [Test]
+        public void Fatal()
+        {
+            log4net.LogManager.GetLogger(typeof(ExceptronAppenderTests)).Fatal("Fatal");
+        }
+        [Test]
+        public void Warn()
+        {
+            log4net.LogManager.GetLogger(typeof(ExceptronAppenderTests)).Warn("Warn");
+        }
+
+
+    }
+}
---
+++ Exceptron.Log4Net/ExceptronAppender.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Exceptron.Client;
+using Exceptron.Client.Configuration;
+using log4net.Core;
+
+namespace Exceptron.Log4Net.Client
+{
+    public class ExceptronAppender : log4net.Appender.AppenderSkeleton
+    {
+        private ExceptionClient _exceptionClient;
+
+        public ExceptronAppender() : base()
+        {
+            var config = new ExceptronConfiguration
+            {
+                ApiKey = ApiKey,
+                ThrowExceptions = ThrowExceptions
+            };
+        }
+
+        /// <summary>
+        /// Exceptron API Key
+        /// </summary>
+        public string ApiKey { get; set; }
+
+        /// <summary>
+        /// If the Appender should also throw exceptions
+        /// </summary>
+        public bool ThrowExceptions { set; get; }
+
+        protected override void Append(log4net.Core.LoggingEvent loggingEvent)
+        {
+            if (loggingEvent == null || loggingEvent.ExceptionObject == null) return;
+
+
+            try
+            {
+                var exceptionData = new ExceptionData
+                {
+                    Exception = loggingEvent.ExceptionObject,
+                    Component = loggingEvent.LoggerName,
+                    Message = loggingEvent.RenderedMessage,
+                };
+
+                if (loggingEvent.Level <= Level.Info)
+                {
+                    exceptionData.Severity = ExceptionSeverity.None;
+                }
+                else if (loggingEvent.Level <= Level.Warn)
+                {
+                    exceptionData.Severity = ExceptionSeverity.Warning;
+                }
+                else if (loggingEvent.Level <= Level.Error)
+                {
+                    exceptionData.Severity = ExceptionSeverity.Error;
+                }
+                else if (loggingEvent.Level <= Level.Fatal)
+                {
+                    exceptionData.Severity = ExceptionSeverity.Fatal;
+                }
+
+                _exceptionClient.SubmitException(exceptionData);
+            }
+            catch (Exception e)
+            {
+                //base.Append(new LoggingEvent(new LoggingEventData() { Domain= loggingEvent.Domain, ExceptionString = e.ToString(), Identity = loggingEvent.Identity, Level = Level.Error, Message = "Unable to report exception." }));
+            }
+        }
+    }
+}
